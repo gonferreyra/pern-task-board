17749aaf79c757428b37a2f4234c9e874963d0e3
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-10 12:30:23 -0300

    renamed function to sequelize

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,18 +1,12 @@
 import { Sequelize } from 'sequelize';
-import {
-  DB_HOST,
-  DB_NAME,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_USER,
-} from '../constants/env.js';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from '../constants/env.js';
 
 const host = DB_HOST;
 const user = DB_USER;
 const database = DB_NAME;
 const password = DB_PASSWORD;
 
-const DB = new Sequelize(database, user, password, {
+export const sequelize = new Sequelize(database, user, password, {
   host: host,
   dialect: 'postgres',
   // show SQL commands on console
@@ -22,11 +16,10 @@ const DB = new Sequelize(database, user, password, {
 // DB connection
 async function dbConnection() {
   try {
-    await DB.sync();
+    await sequelize.sync();
   } catch (error) {
     throw new Error(`Error connecting to DB: ${error.message}`);
   }
 }
-dbConnection();
 
 export default dbConnection;
