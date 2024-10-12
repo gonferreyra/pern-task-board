1862a6caf8372bfc9701d975b73cbb04add97282
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-12 16:11:45 -0300

    envs updated

--- server/src/constants/env.js
+++ server/src/constants/env.js
@@ -17,3 +17,7 @@ export const DB_HOST = getEnv('DB_HOST');
 export const DB_PORT = getEnv('DB_PORT');
 export const DB_USER = getEnv('DB_USER');
 export const DB_PASSWORD = getEnv('DB_PASSWORD');
+
+// JWT
+export const JWT_SECRET = getEnv('JWT_SECRET');
+export const JWT_REFRESH_SECRET = getEnv('JWT_REFRESH_SECRET');
