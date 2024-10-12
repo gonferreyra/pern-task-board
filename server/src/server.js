613e53851e15692f15f39af7df6aeb3d41320cf3
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-12 18:29:30 -0300

    cookie parser middelware added

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { loggerMiddleware } from './middlewares/logger-handler.js';
 import logger from './config/logger.js';
 import { SERVER_HOSTNAME, SERVER_PORT } from './constants/env.js';
@@ -16,6 +17,8 @@ const Main = async () => {
   app.use(express.urlencoded({ extended: true }));
   // JSON - reading and parsing body
   app.use(express.json());
+  // cookie parser middleware
+  app.use(cookieParser()); // parse cookies - req.cookies available
   app.use(
     cors({
       origin: '*',
