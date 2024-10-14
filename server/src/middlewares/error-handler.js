d272f6b1ccf66dbcb8fe5055af72b37030f26ef7
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-14 16:03:25 -0300

    clear cookies on error when refreshing tokens

--- server/src/middlewares/error-handler.js
+++ server/src/middlewares/error-handler.js
@@ -1,7 +1,14 @@
+import { clearAuthenticationCookies } from '../utils/cookies.js';
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   // If the error is an instance of CustomError, we use the custom statusCode, otherwise it defaults to 500.
   const statusCode = err.isCustom ? err.statusCode : 500;
   const message = err.message || 'Internal Server Error';
+
+  // delete cookies if the path is /auth/refresh
+  if (req.path === '/auth/refresh') {
+    clearAuthenticationCookies(res);
+  }
 
   res.status(statusCode).json({
     error: message,
