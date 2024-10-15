36c45e6b7158f4b646482a47c60de10f1887fd32
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-15 13:03:45 -0300

    refactor sendEmail function for better error handling

--- server/src/utils/send-mail.js
+++ server/src/utils/send-mail.js
@@ -2,20 +2,32 @@ import resend from '../config/resend.js';
 import { NODE_ENV } from '../constants/env.js';
 
 // function to make the email sender dynamic IF you have a domain and have set it up on resend -- not working on this project
-const getFromEmail = () => {
-  NODE_ENV === 'development' ? 'onboarding@resend.dev' : EMAIL_SENDER;
-};
+// const getFromEmail = () =>
+//   NODE_ENV === 'development' ? 'onboarding@resend.dev' : EMAIL_SENDER;
+// ;
 
-const getToEmail = (to) => {
+const getToEmail = (to) =>
   NODE_ENV === 'development' ? 'delivered@resend.dev' : to;
-};
 
 export const sendMail = async ({ to, subject, text, html }) => {
-  await resend.emails.send({
-    from: getFromEmail(),
-    to: getToEmail(to),
-    subject,
-    text,
-    html,
-  });
+  try {
+    const response = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: getToEmail(to),
+      subject,
+      text,
+      html,
+    });
+
+    // console.log('Response from Resend:', response);
+
+    if (!response || response.error) {
+      throw response?.error || new Error('Unknown error');
+    }
+
+    return { data: response.data, error: null };
+  } catch (error) {
+    console.error('Exception caught:', err);
+    return { data: null, error: err };
+  }
 };
