efdbc6ee0c80dcaeca2ffa4bfbcc0720964580f9
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-23 11:29:19 -0300

    added custom colors to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     fontFamily: {
       sans: ['Outfit', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        'custom-bg-white': '#f8fafc',
+        'custom-light-green': '#a0ecb1',
+        'custom-dark-green': '#32d657',
+        'custom-pink': '#f7d4d3',
+        'custom-orange': '#e9a23b',
+        'custom-light-orange': '#f5d565',
+        'custom-skin': '#f5e8d5',
+        'custom-red': '#dd524c',
+        'custom-dark-grey': '#97a3b6',
+        'custom-grey': '#00000033',
+        'custom-light-grey': '#e3e8ef',
+        'custom-blue': '#3662e3',
+      },
+    },
   },
   plugins: [],
 };
