562e3beb1c8862d0db05f1595967731a4176c858
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-24 10:27:56 -0300

    send getImgUrl function to utils

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -1,23 +1,11 @@
 import clsx from 'clsx';
+import { getImageUrl } from '../lib/utils';
 
 type TaskProps = {
   name: string;
   description?: string;
   icon: string;
   status: 'completed' | 'in-progress' | 'wont-do' | 'to-do';
-};
-
-const getImageUrl = (status: string) => {
-  switch (status) {
-    case 'completed':
-      return '/Done_round_duotone.svg';
-    case 'in-progress':
-      return '/Time_atack_duotone.svg';
-    case 'wont-do':
-      return '/close_ring_duotone.svg';
-    default:
-      return '/list_duotone.svg';
-  }
 };
 
 function Task({ name, description, icon, status }: TaskProps) {
