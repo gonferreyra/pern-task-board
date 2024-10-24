5c0f87aa58988ce4bac202d0d405939924d318e8
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-24 09:09:05 -0300

    task component created

---
+++ client/src/components/Task.tsx
@@ -0,0 +1,62 @@
+import clsx from 'clsx';
+
+type TaskProps = {
+  name: string;
+  description?: string;
+  icon: string;
+  status: 'completed' | 'in-progress' | 'wont-do' | 'to-do';
+};
+
+const getImageUrl = (status: string) => {
+  switch (status) {
+    case 'completed':
+      return '/Done_round_duotone.svg';
+    case 'in-progress':
+      return '/Time_atack_duotone.svg';
+    case 'wont-do':
+      return '/close_ring_duotone.svg';
+    default:
+      return '/list_duotone.svg';
+  }
+};
+
+function Task({ name, description, icon, status }: TaskProps) {
+  return (
+    <div
+      className={clsx('flex gap-5 rounded-xl px-4 py-4', {
+        'bg-[#f5d565]': status === 'in-progress',
+        'bg-[#F7D4D3]': status === 'wont-do',
+        'bg-[#A0ECB1]': status === 'completed',
+        'bg-[#E3E8EF]': status === 'to-do',
+      })}
+    >
+      <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-[#F8FAFC]">
+        {icon === 'clock' && '⏰'}
+        {icon === 'weight' && '🏋️'}
+        {icon === 'cofee' && '☕️'}
+        {icon === 'books' && '📚'}
+      </div>
+      <div>
+        <h3 className="flex min-h-[45px] items-center text-xl font-semibold">
+          {name}
+        </h3>
+        {description && <p>{description}</p>}
+      </div>
+      <div className="ml-auto flex h-12 w-12 items-center justify-center">
+        {status !== 'to-do' && (
+          <img
+            src={getImageUrl(status)}
+            alt="status"
+            className={clsx('h-12 w-12 rounded-lg p-3', {
+              'bg-[#E9A23B]': status === 'in-progress',
+              'bg-[#32D657]': status === 'completed',
+              'bg-[#DD524C]': status === 'wont-do',
+            })}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Task;
