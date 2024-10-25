5727b1053a877e8d18f98aaa6fdc68a4d336bf3b
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-25 10:48:52 -0300

    authenticate middleware added to createTask function

--- server/src/routes/task.route.js
+++ server/src/routes/task.route.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import * as controllers from '../controllers/task.controller.js';
+import authenticate from '../middlewares/authenticate.js';
 
 const taskRoutes = Router();
 
-taskRoutes.post('/', controllers.createTaskHandler);
+taskRoutes.post('/', authenticate, controllers.createTaskHandler);
 
 export default taskRoutes;
