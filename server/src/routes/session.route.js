281d0311ec9d665082e91e058ed35bff3ba88dde
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-28 10:12:12 -0300

    remove duplicate middleware

--- server/src/routes/session.route.js
+++ server/src/routes/session.route.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import * as controllers from '../controllers/session.controller.js';
-import authenticate from '../middlewares/authenticate.js';
 
 const sessionRoutes = Router();
 
-sessionRoutes.get('/', authenticate, controllers.getSessionHandler);
-sessionRoutes.delete('/:id', authenticate, controllers.deleteSessionHandler);
+sessionRoutes.get('/', controllers.getSessionHandler);
+sessionRoutes.delete('/:id', controllers.deleteSessionHandler);
 
 export default sessionRoutes;
