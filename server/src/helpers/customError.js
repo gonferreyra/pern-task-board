089349694ef268d0ad26ed13c9fb8337a3318d00
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-28 10:00:36 -0300

    CustomError modified and authentica middleware

--- server/src/helpers/customError.js
+++ server/src/helpers/customError.js
@@ -1,7 +1,8 @@
 export class CustomError extends Error {
-  constructor(message, statusCode) {
+  constructor(message, statusCode, errorCode = null) {
     super(message);
     this.statusCode = statusCode;
+    this.errorCode = errorCode;
     this.isCustom = true; // To differentiate custom errors.
   }
 }
