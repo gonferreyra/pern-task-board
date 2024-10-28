ebdd39b670f3ec5a2462dc3cca0ad9597cd21fb1
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-28 09:59:59 -0300

    remove duplicate autheticate middleware

--- server/src/routes/task.route.js
+++ server/src/routes/task.route.js
@@ -5,8 +5,8 @@ import authenticate from '../middlewares/authenticate.js';
 const taskRoutes = Router();
 
 // Edit authenticate on routes after testing
-taskRoutes.post('/', authenticate, controllers.createTaskHandler);
-taskRoutes.get('/', authenticate, controllers.getUserTasksHandler);
+taskRoutes.post('/', controllers.createTaskHandler);
+taskRoutes.get('/', controllers.getUserTasksHandler);
 taskRoutes.put('/:id', controllers.updateTaskHandler);
 taskRoutes.delete('/:id', controllers.deleteTaskHandler);
 
