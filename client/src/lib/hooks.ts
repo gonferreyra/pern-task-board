51d58e6d9d8ff9815b06cd3d4bc77f7fac06d612
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-29 09:27:04 -0300

    useSession and useDeleteSession hooks created

--- client/src/lib/hooks.ts
+++ client/src/lib/hooks.ts
@@ -1,5 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
-import { getUser } from './api';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { deleteSession, getSessions, getUser } from './api';
+import { Session } from './types';
 
 export const useAuth = (options = {}) => {
   const { data: user, ...rest } = useQuery({
@@ -9,4 +10,32 @@ export const useAuth = (options = {}) => {
     ...options,
   });
   return { user, ...rest };
+};
+
+export const useSessions = (options = {}) => {
+  const { data: sessions, ...rest } = useQuery({
+    queryKey: ['sessions'],
+    queryFn: getSessions,
+    ...options,
+  });
+  return { sessions, ...rest };
+};
+
+export const useDeleteSessions = (sessionId: number) => {
+  const queryClient = useQueryClient();
+  const { mutate, ...rest } = useMutation({
+    mutationFn: () => deleteSession(sessionId),
+    onSuccess: () => {
+      // update cache without refetching - optimistic update
+      queryClient.setQueryData(['sessions'], (cache: Session[]) =>
+        cache.filter((session) => session.id !== sessionId),
+      );
+      // refetch and update cache
+      // queryClient.invalidateQueries(['sessions'])
+    },
+  });
+  return {
+    deleteSession: mutate,
+    ...rest,
+  };
 };
