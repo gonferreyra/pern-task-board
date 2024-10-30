31bdb74179020e1c1450e6f4f8978ed7e0c0ff0a
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-30 10:41:17 -0300

    typed navigate params

--- client/src/lib/navigation.ts
+++ client/src/lib/navigation.ts
@@ -1,6 +1,6 @@
-let navigate: () => void;
+let navigate: (path: string, options: any) => void;
 
-export const setNavigate = (fn) => {
+export const setNavigate = (fn: (path: string, options: any) => void) => {
   navigate = fn;
 };
 
